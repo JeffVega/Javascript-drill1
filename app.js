99e011c9dfa52b090724cfdd5b8bbd628da88860
Jeff Vega <jeff@intellibliss.com>
2018-02-19 11:25:44 -0800

    logged year I was born

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function whoIAm(name,age){
+    var yearOfBirth = 2018 - age;
 console.log(`Hi, my name is ${name} and I'm ${age} years old`)
+
+console.log(`I was born in ${yearOfBirth}`)
 };
 whoIAm("jeff",20);
